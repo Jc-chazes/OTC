b8469d5453889e4e84fcfb340a975d6fcc2caa24
Diana Navarro <navarroruiz.diana@gmail.com>
2019-03-01 19:36:23 -0500

    Improve choose access page

--- src/pages/choose-access/choose-access.ts
+++ src/pages/choose-access/choose-access.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, App } from 'ionic-angular';
+import { NavController, NavParams, App, LoadingController } from 'ionic-angular';
 import { Login } from '../login/login';
 import { AppStateService } from '../../providers/app-state.service';
 import { CommonRegisterAccountPage } from '../common-register-account/common-register-account';
@@ -7,13 +7,9 @@ import { AuthProvider } from '../../providers/auth.service';
 import { UsersService } from '../../providers/users.service';
 import { PersonTabsPage } from '../person-tabs/person-tabs';
 import { ExchangeAgentTabsPage } from '../exchange-agent-tabs/exchange-agent-tabs';
-
-/**
- * Generated class for the ChooseAccessPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../models/user.model';
+import { AlertUtil } from '../../providers/utils/alert.util';
+import { RequestResetPasswordPage } from '../request-reset-password/request-reset-password';
 
 @Component({
   selector: 'page-choose-access',
@@ -21,31 +17,27 @@ import { ExchangeAgentTabsPage } from '../exchange-agent-tabs/exchange-agent-tab
 })
 export class ChooseAccessPage {
 
-  constructor(public nvCtrl : NavController, public appState: AppStateService, private auth: AuthProvider,
-    private users: UsersService, private app: App) { }
+  user = new User();
+  showPassword = false;
 
-  ngOnInit() {
-  }
+  constructor(public nvCtrl : NavController, 
+              public appState: AppStateService, 
+              private auth: AuthProvider,
+              private users: UsersService,
+              private loadingCtrl: LoadingController,
+              private alerts : AlertUtil,
+              private app: App) 
+              {
+                this.user.userType = this.appState.currentState.global.userType;
+              }
 
-  login(){
+  ngOnInit() {}
+
+  /* login(){
     this.nvCtrl.push(Login)
-  }
-
-  registerUser(){
-    if( this.appState.currentState.global.userType == '0' ){
-      this.nvCtrl.push(CommonRegisterAccountPage)
-    }else{
-      this.nvCtrl.push(CommonRegisterAccountPage)
-    }
-  }
+  } */
 
   loginWithGoogle(){
-    // this.googlePlus.login()
-    // .then(res => {
-    //   alert(JSON.stringify(res));
-    //   this.user.email = JSON.stringify(res);
-    // })
-    // .catch(err => alert(JSON.stringify(err)));
     this.auth.loginWithGoogle()
     .subscribe( result => {
       if( result.canceled ){
@@ -71,12 +63,6 @@ export class ChooseAccessPage {
   }
 
   loginWithFacebook(){
-    // this.facebook.login(['public_profile', 'user_friends', 'email'])
-    // .then((res: FacebookLoginResponse) => {
-    //   alert(JSON.stringify(res));
-    //   this.user.email = JSON.stringify(res);
-    // })
-    // .catch(e => alert(JSON.stringify(e)));
     this.auth.loginWithFacebook()
     .subscribe( result => {
       if( result.canceled ){
@@ -97,6 +83,40 @@ export class ChooseAccessPage {
         } );
       }
     });
+  }
+
+  login(){
+    if( this.user.password && this.user.email ){
+      let loading = this.loadingCtrl.create();
+      loading.present();
+      this.auth.login(this.user).subscribe( results => {
+        loading.dismiss();
+        if(results){
+          let tabs = null;
+          if( this.users.currentUser.userType == '0' ){
+            tabs = PersonTabsPage;
+          }else {
+            tabs = ExchangeAgentTabsPage;
+          }
+          this.app.getRootNav().setRoot(tabs);
+        }else{
+          this.alerts.show('Credenciales inválidas','Login');
+        }
+      })
+    }
+  }
+
+  forgotPassword($event: any){
+    $event.preventDefault();
+    this.nvCtrl.push(RequestResetPasswordPage);
+  }
+
+  registerUser(){
+    if( this.appState.currentState.global.userType == '0' ){
+      this.nvCtrl.push(CommonRegisterAccountPage)
+    }else{
+      this.nvCtrl.push(CommonRegisterAccountPage)
+    }
   }
 
 }
